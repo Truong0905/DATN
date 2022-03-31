134d00868ce5161c7813d8d8af9d36b7b8981562
Truong <999vantruong@gmail.com>
2022-03-31 12:54:23 +0700

    Fix loi nhan va cong

--- PLC.c
+++ PLC.c
@@ -650,10 +650,15 @@ void Final_File_text(void)
             {
                 p = p->next;
             }
-                            pNEXT = p->next ;
-            if (   (  (strcmp(pNEXT->data, "O") == 0)  || (strcmp(pNEXT->data, "ON") == 0)  )  &&   ((strcmp(pPREV_temp->data, "ALD") != 0) && (strcmp(pPREV_temp->data, "OLD") != 0))   )
+            if (   (  (strcmp(p->data, "O") == 0)  || (strcmp(p->data, "ON") == 0)  )  &&   ((strcmp(pPREV_temp->data, "ALD") != 0) && (strcmp(pPREV_temp->data, "OLD") != 0))   )
             {
-                OUT = str_alloc_and_insert(OUT, insert_str_next);
+                   OUT = str_alloc_and_insert(OUT, insert_str_next);
+                OUT = str_alloc_and_insert(OUT,insert_str_mul) ;
+                 OUT = str_alloc_and_insert(OUT, insert_str_pre);
+                 p = p ->next ;
+                 OUT = str_alloc_and_insert(OUT,p->data) ;
+                 p = p ->next ;
+
             }
 
             if (((strcmp(p->data, "ALD") == 0) || (strcmp(p->data, "OLD") == 0)) && check_H == 1)
@@ -730,10 +735,14 @@ void Final_File_text(void)
                 OUT = str_alloc_and_insert(OUT, insert_str_pre);
                 p = p->next;
             }
-                pNEXT = p->next ;
-            if (   (  (strcmp(pNEXT->data, "A") == 0)  || (strcmp(pNEXT->data, "AN") == 0)  )  &&   ((strcmp(pPREV_temp->data, "ALD") != 0) && (strcmp(pPREV_temp->data, "OLD") != 0))   )
+            if (   (  (strcmp(p->data, "A") == 0)  || (strcmp(p->data, "AN") == 0)  )  &&   ((strcmp(pPREV_temp->data, "ALD") != 0) && (strcmp(pPREV_temp->data, "OLD") != 0))   )
             {
                 OUT = str_alloc_and_insert(OUT, insert_str_next);
+                OUT = str_alloc_and_insert(OUT,insert_str_mul) ;
+                 OUT = str_alloc_and_insert(OUT, insert_str_pre);
+                 p = p ->next ;
+                 OUT = str_alloc_and_insert(OUT,p->data) ;
+                 p = p ->next ;
             }
 
 
